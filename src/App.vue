1c6cfaefb3132152af04267af4f352ac25f40a29
levischouten <levischoutenschool@gmail.com>
2021-07-25 23:33:45 +0200

    Added Tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,25 @@
 <template>
-  <the-header />
-  <the-footer />
+  <div class="flex flex-col justify-evenly items-stretch h-screen font-sans">
+    <the-header class="h-20" />
+    <div class="bg-blue-300 flex-grow">
+      <router-view />
+    </div>
+    <the-footer class="bg-green-300 h-28" />
+  </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import TheHeader from './components/TheHeader.vue';
-import TheFooter from './components/TheFooter.vue';
+import { Options, Vue } from "vue-class-component";
+import TheHeader from "@/components/TheHeader.vue";
+import TheFooter from "@/components/TheFooter.vue";
+import Home from "@/views/Home.vue";
 
 @Options({
   components: {
     TheHeader,
     TheFooter,
+    Home,
   },
 })
 export default class App extends Vue {}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
